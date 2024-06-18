24d5c53f39eb7bf231e222d0110832d4f8d232e1
Fethisensei <fetam62@gmail.com>
2024-06-18 15:53:47 +0200

    docs: add src folder)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { WelcomeModule } from './welcome/welcome.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [WelcomeModule, ShoppingListModule]
 })
 export class AppComponent {
   title = 'my-appp';
